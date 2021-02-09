29b7d3146e6315d04d72b871d2ef9812b762c60d
optimy <optimizion@gmail.com>
2021-02-09 19:48:57 +0900

    Google, Github Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "fBase";
+import { authService, firebaseInstance } from "fBase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -39,6 +39,21 @@ const Auth = () => {
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
 
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const { user, credential } = await authService.signInWithPopup(provider);
+    console.log(user, credential);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -68,8 +83,12 @@ const Auth = () => {
         {newAccount ? "Sign in" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
       </div>
     </div>
   );
